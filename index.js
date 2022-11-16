b7f7bc3b77e2e68f6e67a47c34cb7e0cfc9cc340
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-11-16 11:03:53 -0400

    creando los endpoints

--- index.js
+++ index.js
@@ -2,5 +2,21 @@ import express from 'express';
 
 const app = express();
 
+app.get('/employes', (req, res) => {
+  res.send('Obteniendo empleados');
+});
+
+app.post('/employes', (req, res) => {
+  res.send('Creando empleados');
+});
+
+app.put('/employes', (req, res) => {
+  res.send('Actualizando empleados');
+});
+
+app.delete('/employes', (req, res) => {
+  res.send('Eliminando empleados');
+});
+
 app.listen(3000);
 console.log('Server running on port 3000');
