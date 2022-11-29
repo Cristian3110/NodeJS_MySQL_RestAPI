9af0928bad9344f91dc8cf120fbae5a8be301a04
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-11-29 00:56:39 -0400

    variables de entorno y creación de file app.js ordenado

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+//se coloca .js xq se utiliza módulo de ES6
+import employeesRoutes from './routes/employees.js';
+import indexRoutes from './routes/index.js';
+
+// import { PORT } from './config.js';
+
+const app = express();
+
+// con este método convertimos lo que se le pasa a las rutas con formate JSON
+app.use(express.json());
+
+app.use(indexRoutes);
+app.use('/api', employeesRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).json({
+		msg: 'EndPoint not found',
+	});
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-import express from 'express';
-//se coloca .js xq se utiliza módulo de ES6
-import employeesRoutes from './routes/employees.js';
-import indexRoutes from './routes/index.js';
-
+import app from './app.js';
 import { PORT } from './config.js';
-
-const app = express();
-
-// con este método convertimos lo que se le pasa a las rutas con formate JSON
-app.use(express.json());
-
-app.use(indexRoutes);
-app.use('/api', employeesRoutes);
-
-app.use((req, res, next) => {
-	res.status(404).json({
-		msg: 'EndPoint not found',
-	});
-});
 
 app.listen(PORT);
 console.log('Server running on port', PORT);
